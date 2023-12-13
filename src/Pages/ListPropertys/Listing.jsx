190f4f634bbe064b71f7cf194401897e86da9b28
itzshiv143 <shivsupertech@gmail.com>
2023-12-13 16:28:11 +0530

    list property

--- src/Pages/ListPropertys/Listing.jsx
+++ src/Pages/ListPropertys/Listing.jsx
@@ -8,7 +8,7 @@ const Listing = () => {
         <>
             <SubNavbar />
             <section className="container listing">
-                <h3 className="listing__title">List PG</h3>
+                <h3 className="listing__title">List Property</h3>
                 <div className="row">
                     <div className="col-lg-12 col-md-12 col-sm-12 p-2 justify-center">
                         <div className="row listing__body">
@@ -43,17 +43,17 @@ const Listing = () => {
                                             <label htmlFor="pg-name" className="form-label form__label__listing">Owner Name </label><input type="text" className="form-control" id="pg-name" placeholder="Enter Your Name" />
                                         </div>
                                         <div className="row justify-content-between">
-                                            <div className="col-lg-6">
+                                            <div className="col-lg-6 align-items-between">
                                                 <label  className="form__label__listing">Apartment Type </label>
-                                                <div className="dropdown-center">
+                                                <div className="dropdown-center drop__down">
                                                     
                                                     <button className="dropdown__btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                                        Dropdown button<i className="fa-solid fa-chevron-down" />
+                                                        select<i className="fa-solid fa-chevron-down" />
                                                     </button>
-                                                    <ul className="dropdown-menu">
-                                                        <li><a className="dropdown-item" href="#">Action</a></li>
-                                                        <li><a className="dropdown-item" href="#">Another action</a></li>
-                                                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                                    <ul className="dropdown-menu dropdown__menu__listing">
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">Apartment</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">Independent House/Villa</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">Gated community villa</a></li>
                                                     </ul>
                                                 </div>
 
@@ -66,6 +66,87 @@ const Listing = () => {
                                                 </div>
 
                                             </div>
+                                        </div>
+                                        <div className="row justify-content-between mt-3">
+                                            <div className="col-lg-4 align-items-between">
+                                                <label  className="form__label__listing">BHK Type </label>
+                                                <div className="dropdown-center drop__down">
+                                                    
+                                                    <button className="dropdown__btn dropdown__btn__bhk" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                        select<i className="fa-solid fa-chevron-down" />
+                                                    </button>
+                                                    <ul className="dropdown-menu dropdown__menu__listing__bhk">
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">1 BHK</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">2 BHK</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">3 BHK</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">4 BHK</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">5 BHK</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">5+ BHK</a></li>
+                                                    </ul>
+                                                </div>
+
+
+                                            </div>
+                                            <div className="col-lg-4">
+                                                <div>
+                                                    <label htmlFor="exampleInputEmail1" className="form-label form__label__listing">Total Floor</label>
+                                                    <input type="number" className="form-control listing__email" placeholder="Enter total floor" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                                </div>
+
+                                            </div>
+                                            <div className="col-lg-4">
+                                                <div>
+                                                    <label htmlFor="exampleInputEmail1" className="form-label form__label__listing">Floor</label>
+                                                    <input type="number" className="form-control listing__email" placeholder="Enter floor" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                        <div className="row justify-content-between mt-3">
+                                            <div className="col-lg-6 align-items-between">
+                                                <label  className="form__label__listing">Property age </label>
+                                                <div className="dropdown-center drop__down">
+                                                    
+                                                    <button className="dropdown__btn dropdown__btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                        select<i className="fa-solid fa-chevron-down" />
+                                                    </button>
+                                                    <ul className="dropdown-menu dropdown__menu__listing__property">
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">Less than a year</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">1 to 3 years</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">5 to 10 years</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">More than 10 years</a></li>
+                                                    </ul>
+                                                </div>
+
+
+                                            </div>
+                                            <div className="col-lg-6 align-items-between">
+                                                <label  className="form__label__listing">Facing </label>
+                                                <div className="dropdown-center drop__down">
+                                                    
+                                                    <button className="dropdown__btn dropdown__btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                        select<i className="fa-solid fa-chevron-down" />
+                                                    </button>
+                                                    <ul size="4" className="dropdown-menu dropdown__menu__listing__facing">
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">North</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">South</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">East</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">West</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">North-East</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">North-West</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">South-East</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">South-West</a></li>
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">Don't Know</a></li>
+                                                    </ul>
+                                                </div>
+
+
+                                            </div>
+                                            
+                                            
+                                        </div>
+                                        <div className="mt-3">
+                                            <label htmlFor="pg-name" className="form-label form__label__listing">Built up area </label><input type="text" className="form-control" id="pg-name" placeholder="area sqft." />
                                         </div>
                                     </div>
                                     <div className="tab-pane listing__pg" id="locality-detail" role="tabpanel" aria-labelledby="locality-detail-tab">
@@ -93,6 +174,7 @@ const Listing = () => {
                                         <div className="user__id d-flex justify-content-between align-items-center" />
                                         <div className="personal__info" />
                                     </div>
+                                    
                                 </div>
                             </div>
                         </div >
