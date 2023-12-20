b3b97e6b2436ab50b4d532613a89f04bb593f14a
itzshiv143 <shivsupertech@gmail.com>
2023-12-20 14:22:47 +0530

    pgList half 90% done

--- src/Pages/PGlist/PGlist.jsx
+++ src/Pages/PGlist/PGlist.jsx
@@ -241,14 +241,14 @@ const PGlist = () => {
                                             <div className="col-lg-6 pg__colleges mt-4 ">
                                                 <h2>Gated Closing Time</h2>
                                                 <label htmlFor="time-list">Gated Closing Time</label>
-                                                <input type="time" id="time-list" hidden/>
+                                                <input type="time" id="time-list" hidden />
                                             </div>
                                             <div className="col-lg-6 pg__colleges mt-4">
                                                 <h2>Near Universty/College</h2>
-                                                <input type="text" placeholder="Eg. CU, PU, etc..."/>
+                                                <input type="text" placeholder="Eg. CU, PU, etc..." />
                                             </div>
                                             <div className="col-lg-12 mt-4">
-                                            <label className="form__label__listing">Any direction tip for your tenants</label>
+                                                <label className="form__label__listing">Any direction tip for your tenants</label>
                                                 <textarea name="description" id="description" placeholder="Write a few lines about your property something which is special and makes your property stand out. Please do not mention your contact details in any format  " style={{
                                                     height: "120px",
                                                     width: "100%",
@@ -260,8 +260,118 @@ const PGlist = () => {
                                         </div>
                                     </div>
                                     <div className="tab-pane listing__pg" id="amenities" role="tabpanel" aria-labelledby="amenities-tab">
-                                        <h3>My Payments</h3>
-                                        
+                                        <h3>Provide additional details about your place </h3>
+                                        <hr className="divide" />
+                                        <div className="row pg__list__services">
+                                            <h2 className="col-lg-12">Available Services</h2>
+                                            <div className="list__services col-lg-4">
+                                                <h3>Laundry</h3>
+                                                <div className="col-lg-12 mt-2">
+                                                    <input className="" type="radio" id="food-yes" name="food-ask" defaultValue="email" style={{
+                                                        marginLeft: "0px"
+                                                    }} />
+                                                    <label className="ms-2" htmlFor="food-yes">Yes</label>
+                                                    <input className="" type="radio" id="food-no" name="food-ask" defaultValue="phone" style={{
+                                                        marginLeft: "60px"
+                                                    }} />
+                                                    <label className="ms-2" htmlFor="food-no">No</label>
+                                                </div>
+                                            </div>
+                                            <div className="list__services col-lg-4">
+                                                <h3>Room Cleaning</h3>
+                                                <div className="col-lg-12 mt-2">
+                                                    <input className="" type="radio" id="food-yes" name="food-ask" defaultValue="email" style={{
+                                                        marginLeft: "0px"
+                                                    }} />
+                                                    <label className="ms-2" htmlFor="food-yes">Yes</label>
+                                                    <input className="" type="radio" id="food-no" name="food-ask" defaultValue="phone" style={{
+                                                        marginLeft: "60px"
+                                                    }} />
+                                                    <label className="ms-2" htmlFor="food-no">No</label>
+                                                </div>
+                                            </div>
+                                            <div className="list__services col-lg-4">
+                                                <h3>Warden Facility</h3>
+                                                <div className="col-lg-12 mt-2">
+                                                    <input className="" type="radio" id="food-yes" name="food-ask" defaultValue="email" style={{
+                                                        marginLeft: "0px"
+                                                    }} />
+                                                    <label className="ms-2" htmlFor="food-yes">Yes</label>
+                                                    <input className="" type="radio" id="food-no" name="food-ask" defaultValue="phone" style={{
+                                                        marginLeft: "60px"
+                                                    }} />
+                                                    <label className="ms-2" htmlFor="food-no">No</label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-lg-12 mt-4">
+                                                <label className="form__label__listing">Any direction tip for your tenants</label>
+                                                <textarea name="description" id="description" placeholder="Eg. Take the road after amrita college, take the right after 300m.... " style={{
+                                                    height: "120px",
+                                                    width: "100%",
+                                                    borderRadius: "10px",
+                                                    border: "1px solid grey",
+                                                    padding: "10px"
+                                                }}></textarea>
+                                            </div>
+                                        </div>
+                                        <div className="row amenities__avail">
+                                            <h3 className="mt-4 col-lg-12">PG/Hostel Rules</h3>
+
+                                            <div className="col-lg-4 mt-3 d-flex align-items-center hostel__rules">
+                                                <input type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
+                                                <img src="/image/Vector (7).png" alt="" />
+                                                <label className="ms-3" htmlFor="vehicle1">No Smoking</label><br />
+                                            </div>
+                                            <div className="col-lg-4 mt-3 d-flex align-items-center hostel__rules">
+                                                <input type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
+                                                <img src="/image/Frame 50.png" alt="" />
+                                                <label className="ms-3" htmlFor="vehicle1">No Guardians Stay</label><br />
+                                            </div>
+                                            <div className="col-lg-4 mt-3 d-flex align-items-center hostel__rules">
+                                                <input type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
+                                                <img src="/image/Vector (8).png" alt="" />
+                                                <label className="ms-3" htmlFor="vehicle1">No Drinking </label><br />
+                                            </div>
+                                            <div className="col-lg-4 mt-3 d-flex align-items-center hostel__rules">
+                                                <input type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
+                                                <img src="/image/Vector (9).png" alt="" />
+                                                <label className="ms-3" htmlFor="vehicle1">No Non- Veg</label><br />
+                                            </div>
+                                            <div className="col-lg-4 mt-3 d-flex align-items-center hostel__rules">
+                                                <input type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
+                                                <img src="/image/Vector (10).png" alt="" />
+                                                <label className="ms-3" htmlFor="vehicle1">No Girls Entry</label><br />
+                                            </div>
+                                            <div className="col-lg-4 mt-3 d-flex align-items-center hostel__rules">
+                                                <input type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
+                                                <img src="/image/Group 85.png" alt="" />
+                                                <label className="ms-3" htmlFor="vehicle1">No Girls Entry</label><br />
+                                            </div>
+                                            <div className="col-lg-4 mt-3 d-flex align-items-center hostel__rules">
+                                                <input type="checkbox" id="vehicle1" name="vehicle1" defaultValue="Bike" />
+                                                <img src="/image/Vector (11).png" alt="" />
+                                                <label className="ms-3" htmlFor="vehicle1">No Girls Entry</label><br />
+                                            </div>
+                                        </div>
+                                        <div className="row mt-4">
+                                        <div className="col-lg-6 align-items-between">
+                                                <label className="form__label__listing">Parking </label>
+                                                <div className="dropdown-center drop__down">
+
+                                                    <button className="dropdown__btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                    Select Parking Type<i className="fa-solid fa-chevron-down" style={{
+                                                        paddingLeft:"309px"
+                                                    }} />
+                                                    </button>
+                                                    <ul className="dropdown-menu dropdown__menu__property ">
+                                                        <li><a className="dropdown-item dropdown__item__listing" href="#">Underground</a></li>
+
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div className="tab-pane listing__pg" id="gallery" role="tabpanel" aria-labelledby="gallery-tab">
                                         <h3>Upload photos & videos</h3>
